df7030af506db66f555ec629e8c8c93257b3b51a
Mridul Bansal <bansalmridul2005@gmail.com>
2026-03-17 19:13:49 +0530

    cofigure build

--- Server/app/main.py
+++ Server/app/main.py
@@ -61,7 +61,7 @@ FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 # Setup CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173",FRONTEND_URL], # Vite default ports
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173",FRONTEND_URL,"http://localhost:4173"], # Vite default ports
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
